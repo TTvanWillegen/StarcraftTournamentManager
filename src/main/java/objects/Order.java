624b3045d2d4dee1356cedb2ab3a3259532477be
TTvanWillegen <tobyvanwillegen@hotmail.com>
2017-09-15 23:04:07 +0200

    Add Maven Support

---
+++ src/main/java/objects/Order.java
@@ -0,0 +1,9 @@
+package objects;
+
+/**
+ * @author Toby T. van Willegen
+ * @version 1.1, 2017-06-13.
+ */
+public enum Order {
+	RANDOM, LINEAR
+}
---
+++ src/main/java/objects/match/MatchState.java
@@ -0,0 +1,9 @@
+package objects.match;
+
+/**
+ * @author Toby T. van Willegen
+ * @version 1.1, 2017-06-13.
+ */
+public enum MatchState {
+	IN_LINE, AWAITING_OPPONENT, PLAYING, PLAYED, DISCARDED
+}

5f5acd13a0ccb664d87b05aeede3e9c70a382cca
TTvanWillegen <tobyvanwillegen@hotmail.com>
2017-09-15 23:24:40 +0200

    Remove Maven, Add Gradle

--- src/exceptions/OrderNotDefinedException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-/**
- * @author Toby T. van Willegen
- * @version 1.1, 2017-06-13.
- */
-public class OrderNotDefinedException extends Exception{
-
-}
--- src/objects/match/MatchState.java
+++
@@ -1,9 +0,0 @@
-package objects.match;
-
-/**
- * @author Toby T. van Willegen
- * @version 1.1, 2017-06-13.
- */
-public enum MatchState {
-	IN_LINE, AWAITING_OPPONENT, PLAYING, PLAYED, DISCARDED
-}

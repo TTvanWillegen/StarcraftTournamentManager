54fb7c8233847946bcdbbd0c598db0850414e1d5
TTvanWillegen <tobyvanwillegen@hotmail.com>
2017-09-26 21:23:35 +0200

    Add first Test

--- src/test/java/objects/MapTest.java
+++ src/test/java/objects/MapTest.java
@@ -1,9 +1,9 @@
 package objects;
 
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Toby T. van Willegen

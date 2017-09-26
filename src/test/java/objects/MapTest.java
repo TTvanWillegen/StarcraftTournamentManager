656cc03f03bf3fb1e42aa6d3c36c96fdf63dbc4a
TTvanWillegen <tobyvanwillegen@hotmail.com>
2017-09-26 16:50:37 +0200

    Add JaCoCo / CodeCov

---
+++ src/test/java/objects/MapTest.java
@@ -0,0 +1,19 @@
+package objects;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Toby T. van Willegen
+ * @version 1.1, 2017-09-26.
+ */
+class MapTest {
+    @Test
+    void testEqualsTrue() {
+        Map map1 = new Map("MapA", 1);
+        Map map2 = new Map("MapA", 1);
+        assertTrue(map1.equals(map2));
+    }
+}

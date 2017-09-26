6d38623596723ba4628979e03659db42b1b352ea
TTvanWillegen <tobyvanwillegen@hotmail.com>
2017-09-26 16:11:17 +0200

    Fix Checkstyle. Add GoogleChecks.

--- src/main/java/objects/Map.java
+++ src/main/java/objects/Map.java
@@ -1,62 +1,72 @@
 package objects;
 
+import java.io.File;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+
 /**
+ * Creates an object for a map, containing the name of the map, the map
+ * location aswell as the maximum amount of teams that can play the map.
+ *
  * @author Toby T. van Willegen
  * @version 1.1, 2017-06-13.
  */
 public class Map {
-	private String mapName;
-	private int maxAmountOfTeams;
+    @NonNull
+    private String mapName;
+    @Nullable
+    private File fileName = null;
+    @NonNull
+    private int maxAmountOfTeams;
 
-	public Map(String mapName, int maxAmountOfTeams) {
-		this.mapName = mapName;
-		this.maxAmountOfTeams = maxAmountOfTeams;
-	}
+    public Map(String mapName, int maxAmountOfTeams) {
+        this.mapName = mapName;
+        this.maxAmountOfTeams = maxAmountOfTeams;
+    }
 
-	public String getMapName() {
-		return mapName;
-	}
+    public String getMapName() {
+        return mapName;
+    }
 
-	/**
-	 * Returns the maximum amount of {@link objects.team.Team}s that can
-	 * play on this {@link Map}
-	 *
-	 * @return {@link int} the maximum amount of {@link objects.team.Team}s
-	 */
-	public int getMaxAmountOfTeams() {
-		return maxAmountOfTeams;
-	}
+    /**
+     * Returns the maximum amount of {@link objects.team.Team}s that can
+     * play on this {@link Map}.
+     *
+     * @return {@link int} the maximum amount of {@link objects.team.Team}s
+     */
+    public int getMaxAmountOfTeams() {
+        return maxAmountOfTeams;
+    }
 
-	@Override
-	public boolean equals(@Nullable Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
-		Map map = (Map) o;
+        Map map = (Map) o;
 
-		if (maxAmountOfTeams != map.maxAmountOfTeams) {
-			return false;
-		}
-		return mapName != null ? mapName.equals(map.mapName) : map.mapName ==
-				null;
-	}
+        if (maxAmountOfTeams != map.maxAmountOfTeams) {
+            return false;
+        }
+        return
+            mapName != null ? mapName.equals(map.mapName) : map.mapName == null;
+    }
 
-	@Override
-	public int hashCode() {
-		int result = mapName != null ? mapName.hashCode() : 0;
-		result = 31 * result + maxAmountOfTeams;
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = mapName != null ? mapName.hashCode() : 0;
+        result = 31 * result + maxAmountOfTeams;
+        return result;
+    }
 
-	@Override
-	public String toString() {
-		return "Map{" + "mapName='" + mapName + '\'' + ", maxAmountOfTeams=" +
-			maxAmountOfTeams + '}';
-	}
+    @Override
+    public String toString() {
+        return "Map{" + "mapName='" + mapName + '\'' + ", maxAmountOfTeams="
+                   + maxAmountOfTeams + '}';
+    }
 }
--- src/main/java/objects/match/MatchState.java
+++ src/main/java/objects/match/MatchState.java
@@ -1,9 +1,10 @@
 package objects.match;
 
 /**
+ * Provides a way to show the state of a {@link Match}.
  * @author Toby T. van Willegen
  * @version 1.1, 2017-06-13.
  */
 public enum MatchState {
-	IN_LINE, AWAITING_OPPONENT, PLAYING, PLAYED, DISCARDED
+    IN_LINE, AWAITING_OPPONENT, PLAYING, PLAYED, DISCARDED
 }
